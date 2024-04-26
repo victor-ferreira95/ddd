cf26c20da536eaf9fb17e1656ed13c5a9387ed08
victor_ferreira <vitorsantosferreira10@gmail.com>
2024-04-26 00:03:37 -0300

    feat: entities aways must self-evaluate

--- src/entity/costumer.ts
+++ src/entity/costumer.ts
@@ -1,16 +1,27 @@
 // entidade anêmica -  não tem regra de negocio
 // ORM
+// ter consistências dos dados, refletir ao valo atual do elemento
+// sempre se autoavaliar
 
 class Costumer {
   _id: string
   _name: string
-  _address: string
-  _active: boolean = true
+  _address: string = ''
+  _active: boolean = false
 
-  constructor(_id: string, _name: string, _address: string) {
+  constructor(_id: string, _name: string) {
     this._id = _id
     this._name = _name
-    this._address = _address
+    this.validate()
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error('Name is required')
+    }
+    if (this._id.length === 0) {
+      throw new Error('Id is required')
+    }
   }
 
   // get id() {
@@ -36,9 +47,13 @@ class Costumer {
   // modelagem do domínio rico
   changeName(name: string) {
     this._name = name
+    this.validate()
   }
 
   activate() {
+    if (this._address === '') {
+      throw new Error('Address is mandatory to activate a customer')
+    }
     this._active = true
   }
 
